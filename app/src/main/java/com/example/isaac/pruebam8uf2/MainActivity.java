4e91ff4314756e325b0292219be9f1444d03d233
Isaac Martinez <cf17isaac.martinez@iesjoandaustria.org>
2019-05-01 17:40:24 +0200

    Fargments diseñados y funcionando
    Fragments funcionales

--- app/src/main/java/com/example/isaac/pruebam8uf2/MainActivity.java
+++ app/src/main/java/com/example/isaac/pruebam8uf2/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.isaac.pruebam8uf2;
 
+import android.content.pm.ActivityInfo;
+import android.net.Uri;
+import android.support.v4.app.FragmentTabHost;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -9,16 +13,36 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  implements
+FragmentReportarIncidencia.OnFragmentInteractionListener,
+FragmentListaIncidencias.OnFragmentInteractionListener {
+
+    //Declaracion de usables
     Toolbar toolbar; //Declaramos la toolBar
+    FragmentReportarIncidencia reportarIncidencia;
+    FragmentListaIncidencias listaIncidencias;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Desabilitimamos la rotacion de la pantalla
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //Asociamos las variables a un new fargment (inicializamos)
+        reportarIncidencia = new FragmentReportarIncidencia();
+        listaIncidencias = new FragmentListaIncidencias();
+
+        //Iniciamos la app con el fragment que elijamos
+        getSupportFragmentManager().beginTransaction().add(R.id.contenedorFragment, reportarIncidencia).commit();
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
     }
 
     @Override
@@ -30,17 +54,25 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        //Declaramos una variable de tipo fargment para substituir la pantalla en funcion del boton
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
         switch (item.getItemId()){
             case R.id.musicItem:
                 Toast.makeText(this, "play/pause musica", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.item1:
-                Toast.makeText(this, "Reportar incidencias", Toast.LENGTH_SHORT).show();
+                transaction.replace(R.id.contenedorFragment, reportarIncidencia);
                 break;
             case R.id.item2:
-                Toast.makeText(this, "Ver incidencias", Toast.LENGTH_SHORT).show();
+                transaction.replace(R.id.contenedorFragment, listaIncidencias);
                 break;
         }
+        //Hacemos el commit independientemente del fragment pulsado
+        transaction.commit();
         return super.onOptionsItemSelected(item);
     }
+
+
 }
