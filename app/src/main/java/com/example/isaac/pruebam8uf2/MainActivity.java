4bb484367122303e71db033ddf7bb6df9c3a1c77
Isaac Martinez <cf17isaac.martinez@iesjoandaustria.org>
2019-05-01 16:59:42 +0200

    ToolBar created
    The toolbar is visible but dont have menu or buttons

--- app/src/main/java/com/example/isaac/pruebam8uf2/MainActivity.java
+++ app/src/main/java/com/example/isaac/pruebam8uf2/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.isaac.pruebam8uf2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar; //Declaramos la toolBar
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
