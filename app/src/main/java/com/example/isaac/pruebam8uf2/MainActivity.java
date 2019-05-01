78dd4516b6effdc9a45d1554229fb68b70472920
Isaac Martinez <cf17isaac.martinez@iesjoandaustria.org>
2019-05-01 17:14:05 +0200

    Menu acabado
    Menu de la toolbar acabado, envia un toast en cada boton

--- app/src/main/java/com/example/isaac/pruebam8uf2/MainActivity.java
+++ app/src/main/java/com/example/isaac/pruebam8uf2/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.isaac.pruebam8uf2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +19,28 @@ public class MainActivity extends AppCompatActivity {
 
         toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.musicItem:
+                Toast.makeText(this, "play/pause musica", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item1:
+                Toast.makeText(this, "Reportar incidencias", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.item2:
+                Toast.makeText(this, "Ver incidencias", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
